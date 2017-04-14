6f6e3c96a0f3d6f368fa23127ad9359a5f0d5829
Madeleine Austin <Maddy@MSEAVIDCHAN.local>
2017-04-13 21:14:57 -0700

    Fixed indentation on GoogleService file

--- iOS/Services/GoogleService.cs
+++ iOS/Services/GoogleService.cs
@@ -21,32 +21,33 @@ namespace USCEvents.iOS.Services
 		// at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
 		static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
 		static string ApplicationName = "Google Sheets API .NET Quickstart";
+
 		public GoogleService()
 		{
 			UserCredential credential;
 
-			            using (var stream =
-							new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-			            {
-			                string credPath = System.Environment.GetFolderPath(
-								System.Environment.SpecialFolder.Personal);
-			credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+            using (var stream =
+				new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+					System.Environment.SpecialFolder.Personal);
+				credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
 
-			                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-			                    GoogleClientSecrets.Load(stream).Secrets,
-			                    Scopes,
-			                    "user",
-			                    CancellationToken.None,
-			                    new FileDataStore(credPath, true)).Result;
-			                Console.WriteLine("Credential file saved to: " + credPath);
-			            }
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
 
-			            // Create Google Sheets API service.
-			            var service = new SheetsService(new BaseClientService.Initializer()
-						{
-							HttpClientInitializer = credential,
-							ApplicationName = ApplicationName,
-						});
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = ApplicationName,
+			});
 
 			// Define request parameters.
 			String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
@@ -58,20 +59,20 @@ namespace USCEvents.iOS.Services
 			// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
 			ValueRange response = request.Execute();
 			IList<IList<Object>> values = response.Values;
-			            if (values != null && values.Count > 0)
-			            {
-			                Console.WriteLine("Name, Major");
-			                foreach (var row in values)
-			                {
-			                    // Print columns A and E, which correspond to indices 0 and 4.
-			                    Console.WriteLine("{0}, {1}", row[0], row[4]);
-			                }
-			            }
-			            else
-			            {
-			                Console.WriteLine("No data found.");
-			            }
-			            Console.Read();
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            Console.Read();
 		}
 	}
 
