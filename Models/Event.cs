6c3e108060e633dbbc201a354dbd919de45686b5
Madeleine Austin <Maddy@MSEAVIDCHAN.local>
2017-04-13 22:12:21 -0700

    Updated packages

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,6 +5,7 @@ namespace USCEvents.Models
 	{
 		public string Title { get; set; }
 		public string Venue { get; set; }
+		public string VenueAdress { get; set; }
 		//public string Description { get; set; }
 		//public DateTime StartTime { get; set; }
 		//public DateTime EndTime { get; set; }
