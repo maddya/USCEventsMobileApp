fce93c38ee8e9a8588cfdff8d6f0116b95bd2b93
randimendel <randi.mendel@yahoo.com>
2017-04-21 14:15:21 -0700

    Creating rewards objects

--- Models/Reward.cs
+++ Models/Reward.cs
@@ -3,8 +3,11 @@ namespace USCEvents
 {
 	public class Reward
 	{
-		public Reward()
-		{
-		}
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime ExpDateAndTime { get; set; }
+		public int Points { get; set; }
+		public string Fname { get; set; }
+		public string Lname { get; set; }
 	}
 }
