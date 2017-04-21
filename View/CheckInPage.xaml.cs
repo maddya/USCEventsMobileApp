b40f42e27fb489ef898200a65c5546fee22104e6
DianaGriffing <diana.griffing@gmail.com>
2017-04-21 12:57:34 -0700

    Adds Checkin page and my rewards page (totally empty tho)

---
+++ View/CheckInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace USCEvents
+{
+	public partial class CheckInPage : ContentPage
+	{
+		public CheckInPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
