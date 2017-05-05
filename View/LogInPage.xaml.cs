9bbe1a222226b8d71f11582724ca9342ae88c286
DianaGriffing <diana.griffing@gmail.com>
2017-05-05 10:25:32 -0700

    Adds Login Page

---
+++ View/LogInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace USCEvents
+{
+	public partial class LogInPage : ContentPage
+	{
+		public LogInPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
