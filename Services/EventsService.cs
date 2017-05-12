30372da27d3bfa9acd46a9375bb930b99a19f1ab
Madeleine Austin <Maddy@D-10-18-223-88.dhcp4.washington.edu>
2017-05-12 09:25:16 -0700

    added event details page and updated event service/models

--- Services/EventsService.cs
+++ Services/EventsService.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using USCEvents.Models;
 using Xamarin.Forms;
-using System.Diagnostics;
 
 namespace USCEvents.Services
 {
@@ -23,38 +23,29 @@ namespace USCEvents.Services
 		public async Task<List<Event>> RefreshDataAsync()
 		{
 			var uri = new Uri($"{Configuration.GoogleSheetsDomain}/{Configuration.EventsSpreadsheetId}/values/{Configuration.EventsRangeQuery}?key={Configuration.GoogleSheetsAPIKey}");
-		 	var response = await client.GetAsync(uri);
+			var response = await client.GetAsync(uri);
 			GoogleSheetsResponse Item;
 			List<Event> events = new List<Event>();
-			//Debug.WriteLine("events");
 			if (response.IsSuccessStatusCode)
 			{
-				//Debug.WriteLine("success");
 				var content = await response.Content.ReadAsStringAsync();
 				Item = JsonConvert.DeserializeObject<GoogleSheetsResponse>(content);
 				foreach (var e in Item.values)
 				{
-					events.Add(new Event { 
+					events.Add(new Event
+					{
 						Title = e[0],
 						Venue = e[1],
 						VenueAdress = e[2],
 						Description = e[3],
-						StartDateAndTime = e[4] + " " + e[5],
-						EndDateAndTime = e[6] + " " + e[7],
-						VenueAndDate = e[1] + " - " + e[4],
-						EventImageSource = e[8]});
+						StartDateAndTime = Convert.ToDateTime(e[4] + " " + e[5]),
+						EndDateAndTime = Convert.ToDateTime(e[6] + " " + e[7]),
+						EventImageSource = e[8]
+					});
 				}
-				//Debug.WriteLine("events: " + events);
 				return events;
 			}
 			return null;
-		}
-
-		public Image GetImage()
-		{
-			var webImage = new Image { Aspect = Aspect.AspectFit };
-			webImage.Source = ImageSource.FromUri(new Uri("https://help.seesaw.me/hc/en-us/article_attachments/201790359/student-icon-puppy.png"));
-			return webImage;
 		}
 
 
@@ -64,45 +55,40 @@ namespace USCEvents.Services
 			return events;
 		}
 
-		public List<Event> GetDummyEvents()
-		{
-			var events = new List<Event>();
-			events.Add(new Event { Title = "Bass Academy", 
-				Venue = "Tacoma Dome",
-				VenueAdress = "2727 E D St Tacoma, WA",
-				Description = "Some heavy bass at the academy.",
-				StartDateAndTime = "8/17/2017 9:00PM",
-				EndDateAndTime = "8/18/2017 2:00AM",
-				VenueAndDate = "Tacoma Dome - 8/17/2017"});
-			events.Add(new Event { Title = "Life in Color",
-				Venue = "Washington Mutual",
-				VenueAdress = "800 Occidental Ave S Seattle, WA",
-				Description = "Their life is in color, believe it or not.",
-				StartDateAndTime = "9/10/2017 9:30PM",
-				EndDateAndTime = "9/11/2017 2:00AM",
-				VenueAndDate = "Washington Mutual - 9/10/2017"});
-			events.Add(new Event { Title = "Bliss",
-				Venue = "Tacoma Dome",
-				VenueAdress = "2727 E D St Tacoma, WA",
-				Description = "This show is going to be PURE BLISS.",
-				StartDateAndTime = "10/10/2017 10:00PM",
-				EndDateAndTime = "10/10/2017 10:00PM",
-				VenueAndDate = "Tacoma Dome - 10/10/2017"});
-			events.Add(new Event { Title = "Paradiso",
-				Venue = "The Gorge",
-				VenueAdress = "754 Silica Road NW George, WA",
-				Description = "The biggest music festival of the year.",
-				StartDateAndTime = "9/8/2017 10:00AM",
-				EndDateAndTime = "9/11/2017 8:00PM",
-				VenueAndDate = "The Gorge - 9/8/2017"});
-			events.Add(new Event { Title = "Magnifique",
-				Venue = "The Gorge",
-				VenueAdress = "754 Silica Road NW George, WA",
-				Description = "Another big concert at a sweet venue!",
-				StartDateAndTime = "11/10/2017 8:00PM",
-				EndDateAndTime = "11/12/2017 2:00AM",
-				VenueAndDate = "The Gorge - 11/10/2017"});
-			return events;
-		}
+		//public List<Event> GetDummyEvents()
+		//{
+		//	var events = new List<Event>();
+		//	events.Add(new Event { Title = "Bass Academy", 
+		//		Venue = "Tacoma Dome",
+		//		VenueAdress = "2727 E D St Tacoma, WA",
+		//		Description = "Some heavy bass at the academy.",
+		//		StartDateAndTime = "8/17/2017 9:00PM",
+		//		EndDateAndTime = "8/18/2017 2:00AM"});
+		//	events.Add(new Event { Title = "Life in Color",
+		//		Venue = "Washington Mutual",
+		//		VenueAdress = "800 Occidental Ave S Seattle, WA",
+		//		Description = "Their life is in color, believe it or not.",
+		//		StartDateAndTime = "9/10/2017 9:30PM",
+		//		EndDateAndTime = "9/11/2017 2:00AM"});
+		//	events.Add(new Event { Title = "Bliss",
+		//		Venue = "Tacoma Dome",
+		//		VenueAdress = "2727 E D St Tacoma, WA",
+		//		Description = "This show is going to be PURE BLISS.",
+		//		StartDateAndTime = "10/10/2017 10:00PM",
+		//		EndDateAndTime = "10/10/2017 10:00PM"});
+		//	events.Add(new Event { Title = "Paradiso",
+		//		Venue = "The Gorge",
+		//		VenueAdress = "754 Silica Road NW George, WA",
+		//		Description = "The biggest music festival of the year.",
+		//		StartDateAndTime = "9/8/2017 10:00AM",
+		//		EndDateAndTime = "9/11/2017 8:00PM"});
+		//	events.Add(new Event { Title = "Magnifique",
+		//		Venue = "The Gorge",
+		//		VenueAdress = "754 Silica Road NW George, WA",
+		//		Description = "Another big concert at a sweet venue!",
+		//		StartDateAndTime = "11/10/2017 8:00PM",
+		//		EndDateAndTime = "11/12/2017 2:00AM"});
+		//	return events;
+		//}
 	}
 }
