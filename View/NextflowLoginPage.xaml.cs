1065bdab27a08b56bacf81c461982a99b3845a2e
Diana Griffing <diana.griffing@gmail.com>
2017-05-12 18:59:49 -0700

    FACEBOOK LOGIN WORKS

---
+++ View/NextflowLoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace USCEvents
+{
+	public partial class NextflowLoginPage : ContentPage
+	{
+		public NextflowLoginPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
